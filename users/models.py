c12328e3a47cea0a0708a0753629039786d9ede9
robalford <robertsalford@gmail.com>
2022-08-05 10:40:13 -0700

    add create_auth_token signal, register user model with admin

--- users/models.py
+++ users/models.py
@@ -1,7 +1,9 @@
 from django.db import models
-
 from django.contrib.auth.models import AbstractUser
-from django.db import models
+from django.conf import settings
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from rest_framework.authtoken.models import Token
 
 
 class User(AbstractUser):
@@ -11,3 +13,8 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
 
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
