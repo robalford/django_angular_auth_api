e8770adbf479c85e399a522235a363081a566b5b
robalford <robertsalford@gmail.com>
2022-08-05 09:53:25 -0700

    create custom user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    bio = models.TextField(blank=True)
+    profile_pic = models.ImageField(blank=True)
+
+    def __str__(self):
+        return self.username
+
